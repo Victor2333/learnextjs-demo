0b31a01f81f767833b4d7a5f62e62a8b3e7cdfda
yuntang <yuntang@ebay.com>
2018-08-02 08:55:36 +0800

    add Router

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,15 @@
 import Link from 'next/link'
-
+import config from '../config'
 const linkStyle = {
   marginRight: 15
 }
 
 const Header = () => (
 	<div>
-		<Link href="/">
+		<Link href={`${config.router}/`}>
 		  <a style={linkStyle}>Home</a>
 		</Link>
-		<Link href="/about">
+		<Link href={`${config.router}/about`}>
 		  <a style={linkStyle}>About</a>
 		</Link>
 	</div>
